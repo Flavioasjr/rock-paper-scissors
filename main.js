7580f61d00d4390aa01ddc85d8880802d2a8cd54
Flavio Alves <flavioasjr@gmail.com>
2022-03-01 09:17:29 -0300

    finalizadaa função playGame e mostrando o vencedor de cada rodada

--- main.js
+++ main.js
@@ -1,1 +1,41 @@
-console.log('teste');
+function computerPlay() {
+    const game = ['Rock', 'Paper', 'Scissors'];
+    const numberRandom = () => parseInt(Math.random() * (3 - 0) + 0);
+
+    return game[numberRandom()];
+}
+
+function playGame(playerSelection, computerSelection) {
+    if (typeof playerSelection !== 'string' || typeof computerSelection !== 'string') return 'error';
+
+    const player = playerSelection.toUpperCase();
+    const computer = computerSelection.toUpperCase();
+
+    if (player === 'ROCK') {
+        if(computer === 'ROCK') return 'tied game';
+        if(computer === 'PAPER') return `computer is ${computer}. Computer won the game`;
+        if(computer === 'SCISSORS') return `computer is ${computer}. You won the game`;
+    }
+
+    if (player === 'PAPER') {
+        if(computer === 'PAPER') return 'tied game';
+        if(computer === 'SCISSORS') return `computer is ${computer}. Computer won the game`;
+        if(computer === 'ROCK') return `computer is ${computer}. You won the game`;
+    }
+
+    if (player === 'SCISSORS') {
+        if(computer === 'SCISSORS') return 'tied game';
+        if(computer === 'ROCK') return `computer is ${computer}. Computer won the game`;
+        if(computer === 'PAPER') return `computer is ${computer}. You won the game`;
+    }
+
+    return 'error';
+}
+
+
+
+const computerSelection = computerPlay();
+const playerSelection = 'scissors';
+const winner = playGame(playerSelection, computerSelection);
+
+console.log(winner);

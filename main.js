08abdf318e2bdfe5f7165f3cb51d91825f820c2c
Flavio Alves <flavioasjr@gmail.com>
2022-03-01 10:17:40 -0300

    finalizadaa função game e mostrando o vencedor de cada rodada

--- main.js
+++ main.js
@@ -5,37 +5,64 @@ function computerPlay() {
     return game[numberRandom()];
 }
 
-function playGame(playerSelection, computerSelection) {
+function playRound(playerSelection, computerSelection) {
     if (typeof playerSelection !== 'string' || typeof computerSelection !== 'string') return 'error';
 
-    const player = playerSelection.toUpperCase();
-    const computer = computerSelection.toUpperCase();
+    const player = playerSelection;
+    const computer = computerSelection;
 
     if (player === 'ROCK') {
-        if(computer === 'ROCK') return 'tied game';
-        if(computer === 'PAPER') return `computer is ${computer}. Computer won the game`;
-        if(computer === 'SCISSORS') return `computer is ${computer}. You won the game`;
+        if (computer === 'ROCK') return `computer is ${computer}. tied game`;
+        if (computer === 'PAPER') return `computer is ${computer}. Computer won.`;
+        if (computer === 'SCISSORS') return `computer is ${computer}. Player won.  `;
     }
 
     if (player === 'PAPER') {
-        if(computer === 'PAPER') return 'tied game';
-        if(computer === 'SCISSORS') return `computer is ${computer}. Computer won the game`;
-        if(computer === 'ROCK') return `computer is ${computer}. You won the game`;
+        if (computer === 'PAPER') return `computer is ${computer}. tied game`;
+        if (computer === 'SCISSORS') return `computer is ${computer}. Computer won.`;
+        if (computer === 'ROCK') return `computer is ${computer}. Player won.  `;
     }
 
     if (player === 'SCISSORS') {
-        if(computer === 'SCISSORS') return 'tied game';
-        if(computer === 'ROCK') return `computer is ${computer}. Computer won the game`;
-        if(computer === 'PAPER') return `computer is ${computer}. You won the game`;
+        if (computer === 'SCISSORS') return `computer is ${computer}. tied game`;
+        if (computer === 'ROCK') return `computer is ${computer}. Computer won.`;
+        if (computer === 'PAPER') return `computer is ${computer}. Player won.  `;
     }
 
     return 'error';
 }
 
+function game() {
+    // const playerSelection = 'scissors';
+    let game;
+    let player = 0;
+    let computer = 0;
 
+    for (let i = 0; i < 5; i++) {
+        let computerSelection = computerPlay();
+        let playerSelection = prompt('Choose rock, paper or scissors');
+        computerSelection = computerSelection.toUpperCase();
+        playerSelection = playerSelection.toUpperCase();
 
-const computerSelection = computerPlay();
-const playerSelection = 'scissors';
-const winner = playGame(playerSelection, computerSelection);
+        if(playerSelection !== 'ROCK' && playerSelection !== 'PAPER' && playerSelection !== 'SCISSORS') return alert('Invalid value');
+
+        game = playRound(playerSelection, computerSelection);
+
+        console.log(game);
+        game = game.substr(-13);
+        game = game.trim();
+
+        if(game === 'Player won.') player++;
+        if(game === 'Computer won.') computer++;
+        console.log('Player:', player);
+        console.log('Computer:', computer);
+    }
+
+    if (player > computer) return `Player won.`
+    if (player < computer) return `Computer won.`
+    return 'Tied game';
+}
+
+const winner = game();
 
 console.log(winner);

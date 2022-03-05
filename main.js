7e211f619f2419ed14c66769567026aa052d3f12
Flavio Alves <flavioasjr@gmail.com>
2022-03-05 12:55:56 -0300

    inserindo no JS a visualização pelo navegador

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+const btn = document.querySelector('.buttons');
+const body = document.querySelector('body');
+
 function computerPlay() {
     const game = ['Rock', 'Paper', 'Scissors'];
     const numberRandom = () => parseInt(Math.random() * 3);
@@ -7,58 +10,72 @@ function computerPlay() {
 
 const gameResult = {
     ROCK: {
-        ROCK:  'Tied game',
+        ROCK: 'Tied game',
         PAPER: 'Computer won',
         SCISSORS: 'Player won'
     },
     PAPER: {
-        ROCK:  'Player won',
+        ROCK: 'Player won',
         PAPER: 'Tied game',
         SCISSORS: 'Computer won'
     },
     SCISSORS: {
-        ROCK:  'Computer won',
-        PAPER: 'Player wonn',
+        ROCK: 'Computer won',
+        PAPER: 'Player won',
         SCISSORS: 'Tied game'
     }
 }
 
 function playRound(playerSelection, computerSelection) {
-    if (typeof playerSelection !== 'string' || typeof computerSelection !== 'string') return 'error';
+    if (typeof playerSelection !== 'string' 
+    || typeof computerSelection !== 'string') return 'error';
+
+    playerSelection = playerSelection.toUpperCase();
+    computerSelection = computerSelection.toUpperCase();
 
     return gameResult[playerSelection][computerSelection];
 }
 
-function game() {
-    let game;
-    let playerScore = 0;
-    let computerScore = 0;
+btn.addEventListener('click', e => {
+    const computerSelection = computerPlay();
+    const winnerRound = playRound(e.target.value, computerSelection);
+    const div = document.createElement('div');
 
-    for (let i = 0; i < 5; i++) {
-        let computerSelection = computerPlay();
-        let playerSelection = prompt('Choose rock, paper or scissors');
-        computerSelection = computerSelection.toUpperCase();
-        playerSelection = playerSelection.toUpperCase();
+    div.textContent += `Computer is ${computerSelection} and player is ${e.target.value}.
+     ${winnerRound} this round.`;
+    body.appendChild(div);
+});
 
-        if(playerSelection !== 'ROCK' && 
-        playerSelection !== 'PAPER' && 
-        playerSelection !== 'SCISSORS') return alert('Invalid value');
+// function game() {
+//     let game;
+//     let playerScore = 0;
+//     let computerScore = 0;
 
-        game = playRound(playerSelection, computerSelection);
+//     for (let i = 0; i < 5; i++) {
+//         let computerSelection = computerPlay();
+//         let playerSelection = prompt('Choose rock, paper or scissors');
+//         computerSelection = computerSelection.toUpperCase();
+//         playerSelection = playerSelection.toUpperCase();
 
-        console.log(game);
+//         if(playerSelection !== 'ROCK' && 
+//         playerSelection !== 'PAPER' && 
+//         playerSelection !== 'SCISSORS') return alert('Invalid value');
 
-        if(game === 'Player won') playerScore++;
-        if(game === 'Computer won') computerScore++;
-        console.log('Player:', playerScore);
-        console.log('Computer:', computerScore);
-    }
+//         game = playRound(playerSelection, computerSelection);
 
-    if (playerScore > computerScore) return `Player won.`
-    if (playerScore < computerScore) return `Computer won.`
-    return 'Tied game';
-}
+//         console.log(game);
 
-const winner = game();
+//         if(game === 'Player won') playerScore++;
+//         if(game === 'Computer won') computerScore++;
+//         console.log('Player:', playerScore);
+//         console.log('Computer:', computerScore);
+//     }
 
-console.log(winner);
+//     if (playerScore > computerScore) return `Player won.`
+//     if (playerScore < computerScore) return `Computer won.`
+//     return 'Tied game';
+// }
+
+// const winner = game();
+
+// console.log(winner);
